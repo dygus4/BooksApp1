1ca12ec90f721448737c6ca6f41ae7fb442f1408
Wojciech Dyga <wojtek.dyga2@wp.pl>
2021-04-12 23:33:06 +0200

    ZAD 3 - problem

--- src/js/script.js
+++ src/js/script.js
@@ -37,21 +37,39 @@
   render();
   
 
-  const favoriteBooks =[];
-  console.log('favoriteBooks:',favoriteBooks);
 
   function initActions(){
+
+    
+    const favoriteBooks =[];
+    
+
     //referencja do listy wszystkich elementów .book_image w .booksList
     const booksContainer = document.querySelector(select.containerOf.booksList);
     const booksImage =booksContainer.querySelectorAll(select.booksCover.images);
     //pętla po każdym booksImage
-    for (let image of booksImage) {
+    for (let bookImage of booksImage) {
       //nasłuchiwacz uruchamiający dbclick i zatrzymujący domyślne zachowanie
-      image.addEventListener('dblclick', function(event){
+      bookImage.addEventListener('dblclick', function(event){
         event.preventDefault();
-        image.classList.add(classNames.books.favoriteBook);
-        const idBook = image.getAttribute('data-id');
-        favoriteBooks.push(idBook);
+
+        //znajdź data-id
+        const idBook = bookImage.getAttribute('data-id');
+        //sprwdz czy ksiazka  nie zawiera id oraz nie zawieta favorite
+        if( bookImage.classList.contains(!classNames.books.favoriteBook)){
+        //jeśli warunek prawdziy dodaj favorite
+          bookImage.classList.add(classNames.books.favoriteBook);
+          // jeśli warunek prawdziwy wyślij id do nowej tablicy
+          favoriteBooks.push(idBook);
+        // jeśli ksiazka zawiera id i zawiera favorite
+        } else if ( bookImage.classList.contains(classNames.books.favoriteBook)){
+          //znajdź indeks w tej tablicy
+          const indexOfidBook = favoriteBooks.indexOf(idBook);
+          //usuń indeks znaleziony
+          favoriteBooks.splice(indexOfidBook, 1);
+          //usuń klase favorite
+          bookImage.classList.remove(classNames.books.favoriteBook);
+        }
       });
     }
   }
