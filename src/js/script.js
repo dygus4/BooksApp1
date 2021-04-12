789ba31937fc668c6ebf8be7ee0d9da89883a920
Wojciech Dyga <wojtek.dyga2@wp.pl>
2021-04-12 22:25:47 +0200

    ZAD 2

--- src/js/script.js
+++ src/js/script.js
@@ -9,8 +9,16 @@
     },
     containerOf: {
       booksList: '.books-list',
-    }
+    },
+    booksCover: {
+      images: '.book__image',
+    },
 
+  };
+  const classNames = {
+    books: {
+      favoriteBook: 'favorite'
+    },
   };
   const templates = {
     bookTemplate: Handlebars.compile(document.querySelector(select.templateOf.bookTemplate).innerHTML),
@@ -27,5 +35,26 @@
     }
   }
   render();
-  console.log(render);
+  
+
+  const favoriteBooks =[];
+  console.log('favoriteBooks:',favoriteBooks);
+
+  function initActions(){
+    //referencja do listy wszystkich elementów .book_image w .booksList
+    const booksContainer = document.querySelector(select.containerOf.booksList);
+    const booksImage =booksContainer.querySelectorAll(select.booksCover.images);
+    //pętla po każdym booksImage
+    for (let image of booksImage) {
+      //nasłuchiwacz uruchamiający dbclick i zatrzymujący domyślne zachowanie
+      image.addEventListener('dblclick', function(event){
+        event.preventDefault();
+        image.classList.add(classNames.books.favoriteBook);
+        const idBook = image.getAttribute('data-id');
+        favoriteBooks.push(idBook);
+      });
+    }
+  }
+  initActions();
+
 }
